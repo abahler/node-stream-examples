543f2ff8f90c194cc97222c3948437c94186e910
Alex Bahler <alexbahler@gmail.com>
2016-11-11 03:40:10 +0000

    Update comment to reflect stream names

--- tryIt.js
+++ tryIt.js
@@ -8,7 +8,7 @@ var Logger = require('./logger');
 var numGen = new NumberGen(101);    // As with NumberGen's Buffer instance, you need space for 100 values plus one null
 var logger = new Logger();
 
-// Pipe data from the alphabet stream into the cache.
+// Pipe data from the number generator stream into the logger.
 // TODO: for now, just pipe it from the Readable to the Writable, but later, you need to implement a Transform stream
 numGen.pipe(logger);
 
