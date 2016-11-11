a1a9e2ac7236382f4aa00f2f7943505427166f1a
Alex Bahler <alexbahler@gmail.com>
2016-11-11 02:28:37 +0000

    First draft of numberGen complete

--- numberGen.js
+++ numberGen.js
@@ -4,12 +4,10 @@
 
 var stream = require('stream');
 
-// Output letters from a-z
-function NumberGen(options) {
-    stream.Readable.call(this, options);
-    this._start = 'a';
-    this._end = 'z';
-    this._curr = this._start.charCodeAt(0); // Take _start ('a') and get the ASCII code which is 97.
+function NumberGen(size) {
+    stream.Readable.call(this, size);
+    this.size = size;
+    this.index = 0;
 }
 
 // Sets NumberGen.prototype to the obj, but loses correct context of 'this'
@@ -20,12 +18,12 @@ NumberGen.prototype.constructor = NumberGen;
 
 // _read method is called whenever data is required from the Readable
 NumberGen.prototype._read = function() {
-    var letter = String.fromCharCode(this._curr);
-    var buf = new Buffer(letter, 'utf8');
-    this.push(buf);     // This will trigger the `data` event
-    this._curr++;       // Increase to 98, then 99, then 100...
-    if (letter === this._end) {
-        // Tell the buffer that the data has ended
+    var bufInit = new Buffer(101);  // When passing a #, need to pass size of buffer. 101 = 100 plus null value
+    var buf = Buffer.from(bufInit);
+    this.push(buf);
+    this.index++;
+    if (this.index === this.size) {
+        // Indicate data has ended
         this.push(null);
     }
 };
