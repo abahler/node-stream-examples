b7e6e50a10616ac4968755cd6b1add13dbb11188
Alex Bahler <alexbahler@gmail.com>
2016-11-15 04:46:28 +0000

    Started skeleton of constructor and _transform functions

--- divisibleBy.js
+++ divisibleBy.js
@@ -4,9 +4,30 @@
 
 var stream = require('stream');
 
+// `d` is number to use as divisor
 function DivisibleBy(d) {
-    // > Get list of numbers from Number
-    // If number % d === 0, add to buffer. Else, nothing.
+    stream.Transform.call(this, d);
+    this.d = d;
+    this._value = null;     // Holds value of current number
 }
+
+// Sets NumberGen.prototype to the obj, but loses correct context of 'this'
+DivisibleBy.prototype = Object.create(stream.Transform.prototype);
+// Sets constructor back to NumberGen
+DivisibleBy.prototype.constructor = DivisibleBy;
+
+// Must implement the _transform() method
+DivisibleBy.prototype._transform = function(chunk, encoding, callback) {
+    // Get list of numbers from Number
+    // For each `n` number in Buffer: if n % d === 0, add to buffer.
+    if (this.value % this.d === 0) {
+        if (!this._value) {
+            this._value = chunk;
+        } else {    
+            this._value = Buffer.concat([this._value, chunk]);
+        }
+    }
+    callback();
+};
 
 module.exports = DivisibleBy;
