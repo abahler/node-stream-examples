32ef051adfa5919133bd0cf3d752e127e3efde72
Alex Bahler <alexbahler@gmail.com>
2016-11-15 04:45:56 +0000

    Modified comments

--- tryIt.js
+++ tryIt.js
@@ -1,16 +1,17 @@
 // tryIt.js:
 // Procedural code that implements the custom streams NumberGen, DivisibleBy, and Logger
-// TODO: include Transform module and create instance. 
-//       For now, just make sure the Readable and Writable streams work by piping the former to the latter
 
 var NumberGen = require('./numberGen');
+var DivisibleBy = require('./divisibleBy');
 var Logger = require('./logger');
 var numGen = new NumberGen(101);    // As with NumberGen's Buffer instance, you need space for 100 values plus one null
+var divvy = new DivisibleBy(4); 
 var logger = new Logger();
 
 // Pipe data from the number generator stream into the logger.
 // TODO: for now, just pipe it from the Readable to the Writable, but later, you need to implement a Transform stream
 numGen.pipe(logger);
+// Something like: numGen.pipe(divvy.filterByModulo()).pipe(logger);
 
 logger.on('finish', function() {
     console.log('Logger store: ');
